050c86d295a717c2bbfbc885578056d28b45a33d
George123321 <gosha-dem@ya.ru>
2018-03-22 16:58:46 +0300

    Написал delete

--- DV_HW/1_c_i/1_c_i.c
+++ DV_HW/1_c_i/1_c_i.c
@@ -48,18 +48,58 @@ void list_insert(struct Linked_List *lst, const int data_x, const int data_y) {
     }
 }
 
-struct Node list_pop(struct Linked_List *lst) { // TODO: что-то не так, когда 1 элемент
-    if (lst->end == NULL) {
-        printf("List is empty");
+struct Node list_del(struct Linked_List *lst, struct Node *node) {
+    if (lst->begin == NULL) {
+        printf("List is empty\n");
+        struct Node *p = malloc(sizeof(struct Node));
+        return *p;
+    }
+    if (node == NULL) {
+        printf("Node is null\n");
+        struct Node *p = malloc(sizeof(struct Node));
+        return *p;
+    }
+    struct Node *p = lst->begin;
+    while (p != node) {
+        p = p->next;
+    }
+    /* отдельно обработаем случай, когда один элемент */
+    if (lst->size == 1) {
+        lst->size = 0;
+        lst->begin = NULL;
+        lst->end = NULL;
+        return *p;
+    }
+    /* теперь хотя бы 2 элемента */
+    if (p == lst->begin) {
+        lst->begin = p->next;
+        p->next->prev = NULL;
+        p->next = NULL;
+    }
+    else if (p == lst->end) {
+        lst->end = p->prev;
+        p->prev->next = NULL;
+        p->prev = NULL;
     }
     else {
-        lst->size -= 1;
-        lst->end = lst->end->prev;
-        lst->end->next->prev = NULL;
-        struct Node *res = lst->end->next;
-        free(lst->end->next);
-        lst->end->next = NULL;
-        return *res;
+        p->prev->next = p->next;
+        p->next->prev = p->prev;
+    }
+    p->next = NULL;
+    p->prev = NULL;
+    lst->size -= 1;
+    return *p;
+}
+
+struct Node list_pop(struct Linked_List *lst) {
+    list_del(lst, lst->end);
+}
+
+void list_print(struct Linked_List *lst) {
+    struct Node *p = lst->begin;
+    while (p) {
+        printf("(%d, %d)\n", p->x, p->y);
+        p = p->next;
     }
 }
 
@@ -69,11 +109,14 @@ int main() {
     list_insert(&a, 1, 0);
     list_insert(&a, 5, 2);
     list_insert(&a, 3, 1);
-    list_pop(&a);
-    list_pop(&a);
-    list_pop(&a);
-    list_pop(&a);
-    printf("0");
 
+    struct Node *p = a.begin;
+    while (p) {
+        p = a.begin;
+        list_print(&a);
+        list_del(&a, p);
+    }
+    list_del(&a, NULL);
+    list_print(&a);
     return 0;
 }
